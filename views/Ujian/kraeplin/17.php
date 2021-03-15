8664d167736c808ba1b8f70f4e2008af4922ff08
Zen <Phantom_Ghost@outlook.co.id>
2021-03-15 15:40:25 +0700

    Add files via upload

---
+++ views/Ujian/kraeplin/17.php
@@ -0,0 +1,47 @@
+<script type="text/javascript">
+  var ss = 15;
+
+  function countdown() {
+    ss = ss-1;
+
+    if (ss<0) {
+      document.getElementById('submit').disabled = false;
+      
+      document.getElementById('submit').click();
+    }
+    else {
+      document.getElementById("countdown").innerHTML=ss;
+      window.setTimeout("countdown()", 1700);
+    }
+  }
+  
+</script>
+
+<body onload="countdown()">
+<div class="main-content">
+
+        <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header align-right">
+                    <h4 id="countdown"></h4>
+                  </div>
+                  <div class="card-body">
+                  <?php $attributes = ['id'  =>  'formid']; ?>
+                  <?php echo form_open("Kraeplin/kraeplin_selesai_17",$attributes); ?>
+
+                  <div class="row">
+                      <div class="col-12">
+                      
+                      <?php $this->load->view('Ujian/kraeplin/soal/17'); ?>
+
+                      </div>
+                    </div>
+
+                    <?php echo form_close(); ?>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+</div>
---
+++ views/Ujian/kraeplin/soal/4.php
@@ -0,0 +1,536 @@
+<div class="tab-content" id="nav-tabContent">
+
+<!-- Soal Nomor 1 -->
+<div class="tab-pane fade show active" id="nomor-1" role="tabpanel" aria-labelledby="list-home-list">
+
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">5 + 6</h1>
+<input type="number" name="A11" class="form-control text-center" style="width:40%" autofocus>
+</div>
+</center>
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-2" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>  
+<!-- Soal Nomor 2 -->
+<div class="tab-pane fade" id="nomor-2" role="tabpanel" aria-labelledby="list-profile-list">
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">6 + 4</h1>
+<input type="number" id="kotak2" name="A12" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-3" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- No 3 -->
+<div class="tab-pane fade" id="nomor-3" role="tabpanel" aria-labelledby="list-messages-list">
+
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">4 + 2</h1>
+<input type="number"  name="A13" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-4" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+<!-- Soal Nomor 4  -->
+<div class="tab-pane fade" id="nomor-4" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">2 + 8</h1>
+<input type="number"  name="A14" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-5" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 5  -->
+<div class="tab-pane fade" id="nomor-5" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">8 + 8</h1>
+<input type="number"  name="A15" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-6" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 6  -->
+<div class="tab-pane fade" id="nomor-6" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">8 + 7</h1>
+<input type="number"  name="A16" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-7" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 8  -->
+<div class="tab-pane fade" id="nomor-7" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">7 + 5</h1>
+<input type="number"  name="A17" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-9" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 9  -->
+<div class="tab-pane fade" id="nomor-9" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">5 + 3</h1>
+<input type="number" name="A18"  class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-10" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 10  -->
+<div class="tab-pane fade" id="nomor-10" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">3 + 5</h1>
+<input type="number" name="A19"  class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-11" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+                        
+
+<!-- Soal Nomor 11  -->
+<div class="tab-pane fade" id="nomor-11" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">5 + 7</h1>
+<input type="number"  name="A110" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-12" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 12  -->
+<div class="tab-pane fade" id="nomor-12" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">7 + 8</h1>
+<input type="number"  name="A111" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-14" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 14  -->
+<div class="tab-pane fade" id="nomor-14" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">8 + 7</h1>
+<input type="number"  name="A112" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-15" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 15  -->
+<div class="tab-pane fade" id="nomor-15" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">7 + 4</h1>
+<input type="number"  name="A113" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-17" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 17  -->
+<div class="tab-pane fade" id="nomor-17" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">4 + 8</h1>
+<input type="number" name="A114"  class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-19" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 19  -->
+<div class="tab-pane fade" id="nomor-19" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">8 + 8</h1>
+<input type="number"  name="A115" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-20" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 20  -->
+<div class="tab-pane fade" id="nomor-20" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">8 + 7</h1>
+<input type="number"  name="A116" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-22" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 22  -->
+<div class="tab-pane fade" id="nomor-22" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">7 + 6</h1>
+<input type="number"  name="A117" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-23" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 22  -->
+<div class="tab-pane fade" id="nomor-23" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">6 + 2</h1>
+<input type="number"  name="A118" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-25" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 25  -->
+<div class="tab-pane fade" id="nomor-25" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">2 + 3</h1>
+<input type="number"  name="A119" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-26" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 26  -->
+<div class="tab-pane fade" id="nomor-26" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">3 + 7</h1>
+<input type="number"  name="A120" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-27" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 27  -->
+<div class="tab-pane fade" id="nomor-27" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">7 + 7</h1>
+<input type="number"  name="A121" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-29" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 26  -->
+<div class="tab-pane fade" id="nomor-29" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">7 + 6</h1>
+<input type="number"  name="A122" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-30" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 30  -->
+<div class="tab-pane fade" id="nomor-30" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">6 + 5 </h1>
+<input type="number"  name="A123" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-31" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 31  -->
+<div class="tab-pane fade" id="nomor-31" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">5 + 2</h1>
+<input type="number"  name="A124" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-32" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 32  -->
+<div class="tab-pane fade" id="nomor-32" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">2 + 4</h1>
+<input type="number"  name="A125" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-33" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 33  -->
+<div class="tab-pane fade" id="nomor-33" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">4 + 3</h1>
+<input type="number"  name="A126" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="button" id="list-settings-list" data-toggle="list" href="#nomor-34" value="Next" class="btn btn-primary">
+  </div>
+</div>
+
+</div>
+
+<!-- Soal Nomor 34 -->
+<div class="tab-pane fade" id="nomor-34" role="tabpanel" aria-labelledby="list-settings-list">
+
+                        
+<center>
+<div class="col align-self-center">
+<h1 class="text-center">3 + 2</h1>
+<input type="number"  name="A127" class="form-control text-center" style="width:40%">
+</div>
+</center>
+
+
+<div class="col-xs-12">
+  <div class="text-center mt-2">
+    <input type="submit" id="submit" name="submit" value="Selesai" class="btn btn-primary" disabled style="display: none" aria-hidden="true">
+  </div>
+</div>
+
+</div>
+</div>
